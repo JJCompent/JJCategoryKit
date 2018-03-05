952c60db29b6847d531fc8f34be6d53f42190412
运通 Jin <>
2018-03-05 13:36:07 +0800

    JJView

---
+++ JJCategoryKit/Classes/JJView.h
@@ -0,0 +1,13 @@
+//
+//  JJView.h
+//  JJCategoryKit_Example
+//
+//  Created by 运通 Jin on 2018/3/5.
+//  Copyright © 2018年 运通 Jin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JJView : UIView
+
+@end
---
+++ JJCategoryKit/Classes/JJView.m
@@ -0,0 +1,21 @@
+//
+//  JJView.m
+//  JJCategoryKit_Example
+//
+//  Created by 运通 Jin on 2018/3/5.
+//  Copyright © 2018年 运通 Jin. All rights reserved.
+//
+
+#import "JJView.h"
+
+@implementation JJView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
